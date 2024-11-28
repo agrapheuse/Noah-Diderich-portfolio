4810e9875db276e60c80f6e2070929fa65455401
agrapheuse <noah.diderich@gmail.com>
2024-11-28 22:06:52 +0100

    about me update

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -4,10 +4,21 @@ export function AboutMe() {
       <div className="container mx-auto px-6">
         <h2 className="text-2xl font-bold text-center">About Me</h2>
         <p className="mt-4 text-gray-600 text-center">
-          I’m a passionate software developer specializing in building engaging
-          and scalable web applications. My goal is to create seamless user
-          experiences while continuously learning and growing in the tech
-          industry.
+          I am German and Luxembourgish but I was born and raised in Brussels. I
+          was always passionate about science and technologies. I first wanted
+          to become an astrophysicist so at school, I specialized in languages
+          (French, English and German), but most importantly in physics and
+          mathematics. When I graduated high school, I joined free University of
+          Brussels (ULB) in the field of physics to follow my dream. I got to
+          the second year and my passion for physics started to diminish, I
+          however had a IT class where we discovered Python and I loved it.
+        </p>
+        <p className="mt-4 text-gray-600 text-center">
+          I then decided to make a full 180 and applied to the Karel de Grote
+          Hogeschol (KdG). I moved to Antwerp and started a new life as a
+          developer. Now I have finished my degree and I am still just as
+          invested in IT as I was when I first discovered it. I am specialized
+          in full-stack development but I have good knowledge of data and AI.
         </p>
       </div>
     </section>
