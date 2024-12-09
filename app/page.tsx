5c88e6247be8f22919e40579f674074f4c27e9ae
agrapheuse <noah.diderich@gmail.com>
2024-12-09 22:14:28 +0100

    fixed sticky issue

--- app/page.tsx
+++ app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
 
   return (
     <div ref={vantaRef} className="">
-      <header className="bg-middleground">
+      <header className="bg-middleground min-h-10">
         <div className="container mx-auto px-6 py-10">
           <h1 className="text-6xl font-bold text-background-font-color text-center leading-relaxed text-gray-200 font-[family-name:var(--font-geist-mono)]">
             Noah
@@ -48,33 +48,33 @@ export default function Home() {
             Diderich
           </h1>
         </div>
-        <div className="sticky top-10 z-10 flex justify-between items-center w-auto px-48 m-6 mt-20 bg-middleground ">
-          <a
-            href="#about-me"
-            className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
-          >
-            About Me
-          </a>
-          <a
-            href="#career"
-            className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
-          >
-            My Career
-          </a>
-          <a
-            href="#skills"
-            className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
-          >
-            My Skills
-          </a>
-          <a
-            href="#contact-me"
-            className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
-          >
-            Contact Me
-          </a>
-        </div>
       </header>
+      <div className="sticky top-10 z-10 flex justify-between items-center w-auto px-48 m-6 mt-20 bg-middleground ">
+        <a
+          href="#about-me"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          About Me
+        </a>
+        <a
+          href="#career"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          My Career
+        </a>
+        <a
+          href="#skills"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          My Skills
+        </a>
+        <a
+          href="#contact-me"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          Contact Me
+        </a>
+      </div>
 
       <section className="py-80">
         <HeroSection />
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useRef, useState } from "react";
 import FOG from "vanta/dist/vanta.fog.min";
 import * as THREE from "three";
+import { HeroSection } from "@/components/HeroSection";
+import { Skills } from "@/components/Skills";
 
 export default function Projects() {
   const [vantaEffect, setVantaEffect] = useState(null);
@@ -34,5 +36,47 @@ export default function Projects() {
     };
   }, [vantaEffect]);
 
-  return <div ref={vantaRef} className="min-h-screen"></div>;
+  return (
+    <div ref={vantaRef} className="">
+      <header className="bg-middleground min-h-10">
+        <div className="container mx-auto px-6 py-10">
+          <h1 className="text-6xl font-bold text-background-font-color text-center leading-relaxed text-gray-200 font-[family-name:var(--font-geist-mono)]">
+            Noah
+            <br />
+            Diderich
+          </h1>
+        </div>
+      </header>
+      <div className="sticky top-40 z-10 flex justify-between items-center w-auto px-48 m-6 mt-20 bg-middleground ">
+        <a
+          href="#about-me"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          About Me
+        </a>
+        <a
+          href="#career"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          My Career
+        </a>
+        <a
+          href="#skills"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          My Skills
+        </a>
+        <a
+          href="#contact-me"
+          className="text-gray-200 font-[family-name:var(--font-geist-mono)] hover:underline hover:text-gray-300"
+        >
+          Contact Me
+        </a>
+      </div>
+      <div>
+        <HeroSection />
+        <Skills />
+      </div>
+    </div>
+  );
 }
